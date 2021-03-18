8a75db6a835428baa716dab9922ef079592e3c6a
Nachum Barcohen <38861757+nabaco@users.noreply.github.com>
2021-03-18 10:44:32 +0200

    Improved the setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,18 @@
 XDG_USER_CONFIG_DIR="$HOME/.config"
 stowit="stow -v -R -t"
 
+echo "Backing up default config files"
+if [ -f ~/.bashrc ]; then
+    mv ~/.bashrc ~/.bashrc.bak
+fi
+
+echo "Stowing config files"
 $stowit ~ bash
 $stowit ~ zsh
 $stowit ~ git
 $stowit "$XDG_USER_CONFIG_DIR" config
+
+echo "Downloading latest NeoVim"
+mkdir -p ~/.local/usr/bin
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage ~/.local/usr/bin/nvim
+chmod +x ~/.local/usr/bin/nvim
