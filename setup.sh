bdd7a47e6bc0f303b293c0e688d4fb8855473f0e
Nachum Barcohen <38861757+nabaco@users.noreply.github.com>
2021-12-20 06:36:51 +0200

    Pinned NeoVim version to 0.6.0

--- setup.sh
+++ setup.sh
@@ -13,11 +13,12 @@ wget https://github.com/bbkane/fling/releases/download/v$FLING_VERSION/$FLING_PA
 tar -xvzf $FLING_PACKAGE --directory $HOME/.local/bin/ fling
 rm $FLING_PACKAGE
 
+backups=".bashrc .bash_profile .bash_aliases .profile .xprofile .xinitrc"
+
 echo "Backing up standard dotfiles"
 mkdir -p $HOME/.local/share/dotfiles
-for f in `find bash -type f`; do
-	file="`basename $f`"
-	if [ -f "$HOME/$file" -a ! -L "$HOME/$file" ]; then
+for file in $backups; do
+	if [ -f "$HOME/$file" -a ! -h "$HOME/$file" ]; then
 		mv "$HOME/$file" "$HOME/.local/share/dotfiles/$file"
 	fi
 done
@@ -28,13 +29,15 @@ for dir in `find * -maxdepth 0 -type d -not -name "*."`; do
 	fling link --src-dir $dir --ask false
 done
 
-echo "Downloading latest NeoVim"
+NVIM_VERSION="0.6.0"
+echo "Downloading NeoVim v$NVIM_VERSION"
+
 if [ "`id|grep sudo`" == "" ]; then
 	mkdir -p ~/.local/usr/bin
-	wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O ~/.local/usr/bin/nvim
+	wget https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim.appimage -O ~/.local/usr/bin/nvim
 	chmod +x ~/.local/usr/bin/nvim
 else
 	sudo mkdir -p /usr/local/bin
-	sudo wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O /usr/local/bin/nvim
+	sudo wget https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim.appimage -O /usr/local/bin/nvim
 	sudo chmod +x /usr/local/bin/nvim
 fi
